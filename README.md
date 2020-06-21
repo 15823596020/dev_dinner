32a832df3dc80acf6ad73a1a814853f35da8c750
15823596020 <519312875@qq.com>
2020-06-21 16:05:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# jenkinsCI
+
+#### 介绍
+ci学习
+
+#### 使用说明
+
+1.  安装虚拟环境 python3 -m venv venv
+2.  切换到虚拟环境 . ./venv/bin/activate
+3.  安装依赖的库 pip install -r requirement.txt
+4.  启动服务  sh run.sh
---
+++ run.sh
@@ -0,0 +1,15 @@
+killPython()
+{
+    pid=`ps -ef | grep "orderServer.py" | grep -v grep | awk '{print $2}'`
+    echo "python server Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+      echo "no python server pid alive"
+    else
+      kill -9 $pid
+    fi
+}
+
+killPython
+python orderServer.py &
+sleep 2
